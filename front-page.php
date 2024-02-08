c713b26f43c65a381eff4a9e45c8fa074fc0ed5c
Tawfik95100 <136256077+Tawfik95100@users.noreply.github.com>
2024-02-08 10:32:06 +0100

    Hero (en tête de page)

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+get_header();?>
+
+<main>
+    <section class='header'>
+<?php
+get_template_part('templates_part/hero');
+?>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
---
+++ templates_part/hero.php
@@ -0,0 +1,22 @@
+<div class = 'entete'>
+
+<h1>PHOTOGRAPHE EVENT</h1>
+
+<?php
+
+$image_aleatoire = array(
+    "post_type" => "photos",
+    "posts_per_page" => 1,
+    "orderby" => "rand",
+);
+
+$image_query= new wp_query($image_aleatoire);
+
+if($image_query -> have_posts()) {
+    while($image_query -> have_posts()) {
+        $image_query -> the_post();
+        echo get_the_post_thumbnail(get_the_ID(), "full");
+    } wp_reset_postdata();
+}
+?>
+</div>
